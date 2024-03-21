8e3faaf97c52553f55d3004136bf74c21fe8f35e
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-03-21 17:22:46 +0530

    DEVOPS-49 added workflow encryptionand creating secret

---
+++ create_or_update_repo_secret.py
@@ -0,0 +1,61 @@
+import requests
+import os
+from datetime import datetime
+import pytz
+
+def current_ist_time():
+    """code to return time in IST"""
+    # Get the current time in IST
+    ist = pytz.timezone('Asia/Kolkata')
+    ist_now = datetime.now(ist)
+
+    # Format and print the current time in IST
+    ist_now_formatted = ist_now.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    return ist_now_formatted
+
+
+def create_or_update_organization_secret_github(organization: str, secret_name: str):
+    """
+    Create or update org level secret in GitHub
+    Ref https://docs.github.com/en/rest/actions/secrets?apiVersion=2022-11-28#create-or-update-an-organization-secret
+
+    The token must have the following permission set: organization_secrets:write
+    """
+    encrypted_secret = os.getenv('ENCRYPTED_SECRET')
+    if not encrypted_secret:
+        print("ENCRYPTED_SECRET environment variable is not set or is empty.")
+    print(f'encrypted sec is: {encrypted_secret}')
+    ist_now_formatted = current_ist_time()
+    github_org_secret_endpoint = f"https://api.github.com/orgs/{organization}/actions/secrets/{secret_name}"
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {os.getenv('GH_TOKEN')}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    data = {
+        "encrypted_value": encrypted_secret,
+        "visibility": "all",
+        "key_id": os.getenv('PUBLIC_KEY_ID')
+    }
+    response = requests.put(github_org_secret_endpoint, headers=headers, json=data)
+    if response.status_code == '201':
+        print(f"Secret {secret_name} created {organization} at {ist_now_formatted}")
+    else:
+        print(f"Secret {secret_name} updated on {organization} at {ist_now_formatted}")
+
+
+def main():
+    """To test the code"""
+    # Configuring to read ENCRYPTED_SECRET
+    # encrypted_secret = os.getenv('ENCRYPTED_SECRET')
+    # organization = 'devwithkrishna'
+    # secret_name = 'noidea'
+    organization = os.getenv('organization')
+    secret_name = os.getenv('secret_name')
+
+    # Function call
+    create_or_update_organization_secret_github(organization, secret_name)
+
+if __name__ == "__main__":
+    main()
---
+++ encrypt_using_libnacl.py
@@ -0,0 +1,40 @@
+# Python script includes a function, encrypt, designed to encrypt Unicode strings using public-key cryptography through the PyNaCl library.
+# To utilize this functionality, begin by importing the necessary libraries with the from base64 import b64encode and from nacl import encoding,
+# public statements. Subsequently, define and call the encrypt function, passing a Base64-encoded public key and a Unicode string as parameters.
+# The function then encrypts the provided value using the public key and returns the result in a Base64-encoded format.
+# For example, calling encrypt("aSBhbSBrcmlzaG5hZGhhcwo=", "aSBhbSBrcmlzaG5hZGhhcwo=") demonstrates how to encrypt a sample Unicode string using a specified public key.
+# Ensure proper handling and security of public keys and secret values within your application.
+
+from base64 import b64encode
+from nacl import encoding, public
+import os
+
+def encrypt(public_key: str, secret_value: str) -> str:
+    """Encrypt a Unicode string using the public key."""
+    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+    sealed_box = public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return b64encode(encrypted).decode("utf-8")
+
+def main():
+    public_key = os.environ.get("PUBLIC_KEY")
+    secret_value = os.environ.get("SECRET_VALUE")
+    # public_key = "<public key here for local testing>"
+    # secret_value = "Krishnadhas"
+
+    if not (public_key and secret_value):
+        print("Please set PUBLIC_KEY and SECRET_VALUE environment variables.")
+        exit(1)
+
+    try:
+        encrypted_secret = encrypt(public_key, secret_value)
+        os.system(f'echo "ENCRYPTED_SECRET={encrypted_secret}" >> $GITHUB_ENV')
+        print(f"Encrypted Secret: {encrypted_secret}")
+        print(f"Encrypted secret added as a environment variable")
+        return encrypted_secret
+    except Exception as e:
+        print(f"Error encrypting secret: {e}")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
